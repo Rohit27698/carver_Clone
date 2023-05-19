b33ed3db40b209c06573fb2f9f94750fc15de95f
muskanthapa2000 <7022muskan@gmail.com>
2023-05-19 15:48:22 +0530

    done

--- product.js
+++ product.js
@@ -1,12 +1,9 @@
-import navbar from "./navbar.js";
+// import navbar from "./navbar.js";
 
-    let navbarDiv = document.getElementById("navbar_div");
-    navbarDiv.innerHTML = navbar();
+//     let navbarDiv = document.getElementById("navbar_div");
+//     navbarDiv.innerHTML = navbar();
 
 
-import footer from ".footer.js";
-let footerdiv=document.getElementById("footer")
-footerdiv.innerHTML=footer();
 
 
 
