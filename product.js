8f86c1e580ac821f72a6ac1315a4cc74cc2ad4a7
muskanthapa2000 <7022muskan@gmail.com>
2023-05-19 15:10:34 +0530

    done

--- product.js
+++ product.js
@@ -1,12 +1,12 @@
-// import navbar from "./navbar.js";
+import navbar from "./navbar.js";
 
-//     let navbarDiv = document.getElementById("navbar_div");
-//     navbarDiv.innerHTML = navbar();
+    let navbarDiv = document.getElementById("navbar_div");
+    navbarDiv.innerHTML = navbar();
 
 
-// import footer from ".footer.js";
-// let footerdiv=document.getElementById("footer")
-// footerdiv.innerHTML=footer();
+import footer from ".footer.js";
+let footerdiv=document.getElementById("footer")
+footerdiv.innerHTML=footer();
 
 
 
